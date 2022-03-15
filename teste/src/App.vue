83d6d93fe3b27ae56a6b7f542bcafdf7c643aa1b
Jainy <jainyestefany@gmail.com>
2022-03-15 11:14:43 -0300

    a

--- teste/src/App.vue
+++ teste/src/App.vue
@@ -8,25 +8,13 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  
 }
+</script>
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<style scoped>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
